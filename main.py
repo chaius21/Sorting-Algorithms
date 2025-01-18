dc52da7046824409f38e26909a2d1becee31e8dd
chaius21 <19ccabrera@salesian.surrey.sch.uk>
2025-01-18 16:58:01 +0000

    Added main module

---
+++ main.py
@@ -0,0 +1,11 @@
+#Main
+from merge_sort import merge_sort
+from merge_sort import merge
+
+
+ans = yes
+while ans != "no"
+    print("Welcome to the Merge sort program")
+
+
+    ans = input("Do you want to sort another list? (yes/no): ")
