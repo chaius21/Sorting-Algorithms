eafd61abdfcd915a955186f9c439f4ec082e1fbf
chaius21 <19ccabrera@salesian.surrey.sch.uk>
2025-01-20 22:55:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,29 @@
 from merge_sort import merge_sort
 from merge_sort import merge
 
+def main():
+    print("Welcome to the Merge Sort Program!")
+    while True:
+        print("Options:")
+        print("1. Generate a random list and sort it")
+        print("2. Quit")
+        
+        choice = input("Enter your choice (1 or 2): ")
+        
+        if choice == "2":
+            print("Goodbye!")
+            break
+        elif choice == "1":
+            size = input("Enter the size of the list: ")
+            lower_bound =input("Enter the lower bound: ")
+            upper_bound = input("Enter the upper bound: ")
 
-ans = yes
-while ans != "no"
-    print("Welcome to the Merge sort program")
+            random_list = random_list(size, lower, upper)
+            print("Generated List:")
+            print_list(random_list)
 
-
-    ans = input("Do you want to sort another list? (yes/no): ")
+            sorted_list = merge_sort(random_list)
+            print("Sorted List:")
+            print_list(sorted_list)
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
